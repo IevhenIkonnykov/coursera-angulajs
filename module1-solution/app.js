83a46a67fcbe2f591682542c330c988be67db331
yevhen.ikonnykov <yevhen@hummeling.com>
2016-11-12 15:50:56 +0200

    Module 1 assignment

---
+++ module1-solution/app.js
@@ -0,0 +1,33 @@
+(function (){
+  'use strict';
+
+  angular.module ('LunchCheck', [])
+  . controller ('LunchCheckController', LunchCheckController);
+
+  LunchCheckController.$inject = ['$scope'];
+  function LunchCheckController ($scope){
+    $scope.message = '';
+    $scope.lunchMenu = '';
+
+    $scope.checkMenu = function () {
+      var menu = [];
+      var quantity = 0;
+      if ($scope.lunchMenu == "") {
+        $scope.message = 'Please enter data first';
+      } else {
+        menu = $scope.lunchMenu.split(',');
+        for (var i = 0; i < menu.length; i++){
+          if (menu[i].trim() != ""){
+            quantity++;
+          }
+        }
+        if (quantity > 3) {
+          $scope.message = 'Too much!';
+        } else {
+          $scope.message = 'Enjoy!'
+        }
+      }
+    }
+  }
+
+})();
